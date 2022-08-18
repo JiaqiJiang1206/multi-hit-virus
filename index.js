0020713f510938e98c4a3388f9a8569c3d71c3d7
JiaqiJiang1206 <108380174+JiaqiJiang1206@users.noreply.github.com>
2022-08-18 15:35:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-const server = app.listen(3000);
+const server = app.listen(80);
 const io = require('socket.io')(server);
 
 // app.get("/", (req, res) => {
