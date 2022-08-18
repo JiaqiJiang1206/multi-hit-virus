8da242be5fe57d094ef1d491710266b5707e1f53
jiaqi <JiaqiJiang1206@gmail.com>
2022-08-18 11:51:50 +0800

    8_18_2

--- public/sketch.js
+++ public/sketch.js
@@ -117,74 +117,71 @@ function draw(){
 	fill('#03A9F4');
 	noStroke();
 
-	if(gameState){
-
-	}
-		if(pose){
-			let d = dist(pose.leftElbow.x, pose.leftElbow.y, pose.rightElbow.x, pose.rightElbow.y);//计算左右手肘的距离
-			if(d>450){
-				bulState = 0;
-				x = 320+cha-ran-3;
-				if( x < 10 & x > -10){
-					bulState = 1;
-				}
-				dan = 0;
-				
-				if(cha<115){
-					cha++;
-					needleState = 1;
-				}else{
-					cha = 115;
-					needleState = 1;//针管满了
-				}
+	if(pose){
+		let d = dist(pose.leftElbow.x, pose.leftElbow.y, pose.rightElbow.x, pose.rightElbow.y);//计算左右手肘的距离
+		if(d>450){
+			bulState = 0;
+			x = 320+cha-ran-3;
+			if( x < 10 & x > -10){
+				bulState = 1;
 			}
-
-			if(d<350){
-				if(cha>0){
-					cha = 0;
-					needleState == 0;//针管空了
-				}else{
-					cha = 0;
-					needleState == 0;//针管空了
-				}
-				if(cha == 0 & needleState == 1 & bulState == 1){
-						dan += 2;
-						if(gameState){
-							ellipse(width/3+89, 305-dan, 10);//画子弹
-						}
-						
-					
-				}
-				
-			}
-			let d1 = dist(width/3+89, 305-dan, width/3+100, 180-virusR);//计算子弹和病毒的距离
-			if(d1 < 30 & needleState == 1){
-				ran = random(330, 420);
-				needleState = 0;
-				// virusR += 10;//放到服务端上
-				socket.emit('virus', data);
-				// console.log(virusR);
-
-			}
-			// console.log(d1);
-			if(200-virusR > 165){//根据病毒大小判定游戏结束
-				image(evilVirus, width/3+virusR/2, 0, 200-virusR, 200-virusR);//显示正上方的病毒
+			dan = 0;
+			
+			if(cha<115){
+				cha++;
+				needleState = 1;
 			}else{
-				image(boom, width/3+virusR/2, 0, 200-virusR, 200-virusR);//显示爆炸效果
-				gameState = 0;
+				cha = 115;
+				needleState = 1;//针管满了
 			}
-			if(gameState){
-				rect(width/3+77, 320, width/3+80+20, 320+cha);//画注射剂药剂,cha = 87则满
+		}
+
+		if(d<350){
+			if(cha>0){
+				cha = 0;
+				needleState == 0;//针管空了
+			}else{
+				cha = 0;
+				needleState == 0;//针管空了
+			}
+			if(cha == 0 & needleState == 1 & bulState == 1){
+					dan += 2;
+					if(gameState){
+						ellipse(width/3+89, 305-dan, 10);//画子弹
+					}
+					
+				
 			}
 			
-	
+		}
+		let d1 = dist(width/3+89, 305-dan, width/3+100, 180-virusR);//计算子弹和病毒的距离
+		if(d1 < 30 & needleState == 1){
+			ran = random(330, 420);
+			needleState = 0;
+			// virusR += 10;//放到服务端上
+			socket.emit('virus', data);
+			// console.log(virusR);
+
+		}
+		// console.log(d1);
+		if(200-virusR > 165){//根据病毒大小判定游戏结束
+			image(evilVirus, width/3+virusR/2, 0, 200-virusR, 200-virusR);//显示正上方的病毒
+		}else{
+			image(boom, width/3+virusR/2, 0, 200-virusR, 200-virusR);//显示爆炸效果
+			gameState = 0;
 		}
 		if(gameState){
-			image(needle, width/3+25, 280, 130, 200);//显示正下方的注射器
-			strokeWeight(3);
-			fill('red');
-			rect(width/3+77, ran, width/3+80+20, ran+3);//判定线
+			rect(width/3+77, 320, width/3+80+20, 320+cha);//画注射剂药剂,cha = 87则满
 		}
+		
+
+	}
+	if(gameState){
+		image(needle, width/3+25, 280, 130, 200);//显示正下方的注射器
+		strokeWeight(3);
+		fill('red');
+		rect(width/3+77, ran, width/3+80+20, ran+3);//判定线
+	}
 		
 
 	
